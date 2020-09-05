6dd3f1510e4a2e41e528bf43aaf0ec59fc6af539
Дамјан Георгиевски <gdamjan@gmail.com>
2020-09-05 02:07:15 +0200

    arch based rootfs

--- build.sh
+++ build.sh
@@ -4,37 +4,19 @@ set -Eeuo pipefail
 
 PORTABLE_IMAGE=${1:-tt-rss.raw}
 TMPDIR=${TMPDIR:-/tmp}
-DISTRO=focal
-MIRROR=http://archive.ubuntu.com/ubuntu/
 
 mapfile -t PACKAGES < packages.list
-EXCLUDE_PACKAGES=(e2fsprogs fdisk sysvinit-utils login bsdutils)
 
-set +u
-if [ -z "$2" ]; then
+if [ -z "${2:-}" ]; then
     WORKDIR=`mktemp -d -p $TMPDIR`
 else
     WORKDIR=$2
     mkdir -p $WORKDIR
 fi
-set -u
 
-## bootstrap a minimal ubuntu + uwsgi + php
-debootstrapArgs=(
-    --arch=amd64
-    --variant=minbase
-    --components=main,universe
-    --exclude=$(IFS=',' ; echo "${EXCLUDE_PACKAGES[*]}")
-    --include=$(IFS=',' ; echo "${PACKAGES[*]}")
-)
-if [ -n "${CACHE_DIR:-}" ]; then
-    mkdir -p $CACHE_DIR
-    debootstrapArgs+=(--cache-dir=$CACHE_DIR)
-fi
-
-debootstrap \
-    "${debootstrapArgs[@]}" \
-    $DISTRO $WORKDIR $MIRROR
+## bootstrap a minimal arch + uwsgi + php
+pacstrap -c -C pacman.conf \
+    $WORKDIR ${PACKAGES[*]}
 
 
 ## download and unpack tiny tiny rss
@@ -43,6 +25,7 @@ tar xf /tmp/tt-rss.tgz -C $WORKDIR/srv
 
 
 ## copy config files
+mkdir -p $WORKDIR/etc/systemd/system/
 cp files/tt-rss.ini $WORKDIR/srv
 cp files/tt-rss.service files/tt-rss.socket files/tt-rss-update.service $WORKDIR/etc/systemd/system/
 cp files/config.php $WORKDIR/srv/tt-rss
