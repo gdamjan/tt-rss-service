d81c91f69d2329aaf959aa6e71c9ed7991ffb586
Дамјан Георгиевски <gdamjan@gmail.com>
2020-09-10 01:08:29 +0200

    test wip

--- default.nix
+++ default.nix
@@ -2,20 +2,20 @@
 
 let
     tt-rss = pkgs.stdenv.mkDerivation rec {
-      pname = "tt-rss";
-      version = "2020-08-29";
-      rev = "67f02e2aa7b246ef7ca2b2aa4c62e2826327d219";
+        pname = "tt-rss";
+        version = "2020-08-29";
+        rev = "67f02e2aa7b246ef7ca2b2aa4c62e2826327d219";
 
-      src = pkgs.fetchurl {
-        url = "https://git.tt-rss.org/git/tt-rss/archive/${rev}.tar.gz";
-        sha256 = "12gybaawwx8pgshj7lcpwm98waj7cqyy2jp5hsa3m9bnsd23rhld";
-      };
+        src = pkgs.fetchurl {
+          url = "https://git.tt-rss.org/git/tt-rss/archive/${rev}.tar.gz";
+          sha256 = "12gybaawwx8pgshj7lcpwm98waj7cqyy2jp5hsa3m9bnsd23rhld";
+        };
 
-      installPhase = ''
-        mkdir $out
-        cp -ra * $out/
-        cp ${./files/config.php} $out/config.php
-      '';
+        installPhase = ''
+          mkdir $out
+          cp -ra * $out/
+          cp ${./files/config.php} $out/config.php
+        '';
     };
 
     php = (pkgs.php.override {
@@ -31,12 +31,12 @@ let
 
     uwsgi-php = pkgs.uwsgi.override {
         withPAM = false;
-        withSystemd = false;
+        withSystemd = true;
         plugins = ["php"];
         php = php;
     };
 
-    squash-compression = "xz -Xdict-size 100%";
+    squash-compression = "zstd";
 
 in pkgs.stdenv.mkDerivation {
   name = "tt-rss.raw";
