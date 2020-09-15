74acb7b397004fa93ab21b8093322751b751400a
Дамјан Георгиевски <gdamjan@gmail.com>
2020-09-15 03:10:10 +0200

    update version

--- tt-rss.nix
+++ tt-rss.nix
@@ -2,12 +2,12 @@
 
 pkgs.stdenv.mkDerivation rec {
     pname = "tt-rss";
-    version = "2020-08-29";
-    rev = "67f02e2aa7b246ef7ca2b2aa4c62e2826327d219";
+    version = "2020-09-15";
+    rev = "4a074111b5bce126724bf06c9dc83880432e74c9";
 
     src = pkgs.fetchurl {
       url = "https://git.tt-rss.org/git/tt-rss/archive/${rev}.tar.gz";
-      sha256 = "12gybaawwx8pgshj7lcpwm98waj7cqyy2jp5hsa3m9bnsd23rhld";
+      sha256 = "0yyr4lhrs2vncmsdwfdin5fcb1hjgjidvdr296d3af2wz8gsknkk";
     };
 
     phpBin = php + "/bin/php";
