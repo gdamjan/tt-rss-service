7f1e2762baf037fe741bb581bd4ee1dfef8c518d
Дамјан Георгиевски <gdamjan@gmail.com>
2020-11-18 14:07:38 +0100

    bump tt-rss to 2020-11-14 328d7b55

--- tt-rss.nix
+++ tt-rss.nix
@@ -2,9 +2,9 @@
 
 pkgs.stdenv.mkDerivation rec {
     pname = "tt-rss";
-    version = "2020-10-30";
-    rev = "5738e422b5b8e4cf71559f4af39626240acde15f";
-    sha256 = "18hmgcgi11qx9ghkng9bdci623j3abdx9ncggkd0dl29b5dih5zv";
+    version = "2020-11-14";
+    rev = "328d7b55c8cb4d56492356a73df19f7c59784d82";
+    sha256 = "0im8di962y22v767hffgwf1bvhwrv1x5pphlphv4gh9y050w8pxd";
 
     src = pkgs.fetchurl {
       url = "https://git.tt-rss.org/git/tt-rss/archive/${rev}.tar.gz";
