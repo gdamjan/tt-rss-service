e51a4cabbc17eb30ba4af1229ae506d670030634
Дамјан Георгиевски <gdamjan@gmail.com>
2021-07-05 16:46:30 +0200

    bump tt-rss to 2021-06-28 9669bb94d

--- tt-rss.nix
+++ tt-rss.nix
@@ -2,9 +2,9 @@
 
 pkgs.stdenv.mkDerivation rec {
     pname = "tt-rss";
-    version = "2021-05-28";
-    rev = "92c78beb909d8955657564127c2e953ca25113e3";
-    sha256 = "108qhzf1qsn18ym7cxc2ia3a6g6aaig5vw6zvh1qqni16d2p4jb2";
+    version = "2021-06-28";
+    rev = "9669bb94ded6aec423cf307473ade88f4fe71ab7";
+    sha256 = "1pflkmfgb6c8gkz9h4im18zq18gx3v47k83r8vh0wjpafhby5nm7";
 
     src = pkgs.fetchurl {
       url = "https://git.tt-rss.org/git/tt-rss/archive/${rev}.tar.gz";
