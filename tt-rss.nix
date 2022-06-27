cf625e4b0268ba732b18ad024aed21ca23832bf0
Дамјан Георгиевски <gdamjan@gmail.com>
2022-06-27 12:13:56 +0200

    bump tt-rss to 2022-06-20 b148d2f51

--- tt-rss.nix
+++ tt-rss.nix
@@ -2,9 +2,9 @@
 
 pkgs.stdenv.mkDerivation rec {
     pname = "tt-rss";
-    version = "2022-05-30";
-    rev = "d391a01de7cb8509cf92fa005b51b282f34c39d7";
-    sha256 = "sha256-ELC36yIIFJl4ne9wwphkM4ulQDU6sbdVPYpD6wWCkOM=";
+    version = "2022-06-20";
+    rev = "b148d2f5153f9711120b3a3ac50ee84509c9cdfb";
+    sha256 = "sha256-oX07AsIcHUilFidTxpgpAdOztBRwIhnLW7fzQ9k+vS0=";
 
     src = pkgs.fetchgit {
       url = "https://git.tt-rss.org/fox/tt-rss.git";
