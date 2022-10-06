c33c9105c9216a10427c3ddb829007fff8540092
Дамјан Георгиевски <gdamjan@gmail.com>
2022-10-06 13:20:49 +0200

    Update tt-rss.nix

--- tt-rss.nix
+++ tt-rss.nix
@@ -4,7 +4,7 @@ pkgs.stdenv.mkDerivation rec {
     pname = "tt-rss";
     version = "2022-10-01";
     rev = "68dee4578230306da895d5090b7997d8dd23952e";
-    sha256 = "sha256-aX4nn/06QKgyQ9FDBH5yAG+6zJQMFPoCd7NBRIfZ9Io=";
+    sha256 = "sha256-n0uk7S7aJO9PXCcqCQDLe1ear9xPcFrzjjCdt0DnId8=";
 
     src = pkgs.fetchgit {
       url = "https://git.tt-rss.org/fox/tt-rss.git";
