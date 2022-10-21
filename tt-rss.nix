08012141f6fbd179fafaa781b39c1ad21180883c
Дамјан Георгиевски <gdamjan@gmail.com>
2022-10-21 20:36:06 +0200

    Update tt-rss.nix

--- tt-rss.nix
+++ tt-rss.nix
@@ -4,7 +4,7 @@ pkgs.stdenv.mkDerivation rec {
     pname = "tt-rss";
     version = "2022-10-15";
     rev = "602e8684258062937d7f554ab7889e8e02318c96";
-    sha256 = "sha256-n0uk7S7aJO9PXCcqCQDLe1ear9xPcFrzjjCdt0DnId8=";
+    sha256 = "sha256-vgRaxo998Gx9rVeZZl52jppK1v11jpEK0J0NoDMT44I=";
 
     src = pkgs.fetchgit {
       url = "https://git.tt-rss.org/fox/tt-rss.git";
